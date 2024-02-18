85b6cce5e78aa992f141dd34d1a4dd9dfe3b2a00
0xZaddyy <oyinoladapo5@gmail.com>
2024-02-18 17:37:03 +0100

    improved code base

--- block.js
+++ block.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
+const { CLIENT_RENEG_LIMIT } = require('tls');
 
 function parseTransaction(line) {
-  const [txid, fee, weight, parentTxids] = line.split(',');
+  const [txid, fee, weight, ...parentTxids] = line.split(",");
   return {
-    txid,
-    fee: parseInt(fee),
-    weight: parseInt(weight),
-    parentTxids: parentTxids.length > 0 ? parentTxids.split(';') : [],
+      txid,
+      fee: parseInt(fee),
+      weight: parseInt(weight),
+      parentTxids: [
+          ...parentTxids,
+          parentTxids[parentTxids.length - 1].slice(
+              0,
+              parentTxids[parentTxids.length - 1].length - 1
+          ),
+      ],
   };
 }
 
@@ -16,53 +23,34 @@ function readMempool(file) {
   return lines.map(parseTransaction);
 }
 function calculateMaxFeeTransactions(transactions) {
-  const transactionMap = new Map(transactions.map((tx) => [tx.txid, tx]));
+  const sortedTx = transactions.sort((a, b) => b.fee - a.fee);
 
-  const dp = new Map();
+  let totalTnxPossible = [];
+  for (let i = 0; i < sortedTx.length; i++) {
+      const currentWeight = totalTnxPossible.reduce(
+          (acc, cur) => cur.fee + acc,
+          0
+      );
+      if (currentWeight + sortedTx[i].fee > 4000000) {
+          break;
+      } else {
 
-  function calculateMaxFee(txid) {
-    if (dp.has(txid)) {
-      return dp.get(txid);
-    }
-
-    const transaction = transactionMap.get(txid);
-    if (!transaction) {
-      return 0;
-    }
-
-    let maxFee = transaction.fee;
-
-    for (const parentTxid of transaction.parentTxids) {
-      maxFee += calculateMaxFee(parentTxid);
-    }
-
-    dp.set(txid, maxFee);
-
-    return maxFee;
+          totalTnxPossible.push(sortedTx[i]);
+      }
   }
-
-  const result = [];
-
-  transactions.sort(
-    (a, b) => calculateMaxFee(b.txid) - calculateMaxFee(a.txid)
-  );
-
-  let currentWeight = 0;
-
-  for (const transaction of transactions) {
-    if (currentWeight + transaction.weight <= 400000) {
-      result.push(transaction.txid);
-      currentWeight += transaction.weight;
-    }
-  }
-
-  return result;
+totalTnxPossible.reduce((acc,cur)=>cur.fee + acc,0)
+console.log(totalTnxPossible.reduce((acc,cur)=>cur.fee + acc,0))
+totalTnxPossible.map(txn=>txn.txid).join("\n")
+console.log(totalTnxPossible.map(txn=>txn.txid.slice(1)).join("\n"))
+  return totalTnxPossible;
 }
 function main() {
   const file = 'mempool.csv';
+  
   const transactions = readMempool(file);
   const result = calculateMaxFeeTransactions(transactions);
-  console.log(result.join('\n'));
+  // console.log(result.join('\n'));
 }
 
 main();
+
